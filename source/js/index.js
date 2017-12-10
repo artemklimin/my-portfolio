48b4fbeaed8fd3b3cbf466ba47188cb366fd2ca7
AVKlimin <avklimin@yandex.ru>
2017-12-10 11:31:23 +0300

    Some mistake, need a help

--- source/js/index.js
+++
@@ -1,3 +0,0 @@
-import createMenu from './menu';
-var menu = createMenu(['Главная555','Обо мне','Портфолио','Блог'],'menu');
-document.body.appendChild(menu);
--- webpack.config.js
+++ webpack.config.js
@@ -4,26 +4,52 @@ const PATHS = {
     source: path.join(__dirname, 'source'),
     build: path.join(__dirname, 'build')
 };
-module.exports = {
-    entry: PATHS.source + '/js/index.js',
+const common = {
+    entry: {
+    'index': PATHS.source + '/pages/index/index.js',
+    'blog': PATHS.source + '/pages/blog/blog.js'
+    },
     output: {
         path: PATHS.build,
-        filename: 'js/[name].js'
-        },
+        filename: './js/[name].js'
+    },
     plugins: [
         new HtmlWebpackPlugin({
-        template: PATHS.source + '/index.pug',
+            filename: 'index.html',
+            chunks: ['index'],
+            template: PATHS.source + '/pages/index/index.pug'
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'blog.html',
+            chunks: ['blog'],
+            template: PATHS.source + '/pages/blog/blog.pug'
         })
     ],
     module: {
         rules: [
         {
-            test: /\.pug$/,
-            loader: 'pug-loader',
-            options: {
-                pretty: true
-            }
-        }
-        ]
+        test: /\.pug$/,
+        loader: 'pug-loader',
+        options: {
+        pretty: true } 
+        } 
+        ] 
+    } 
+};
+const developmentConfig = {
+    devServer: {
+        stats: 'errors-only'
+    }
+};
+module.exports = function(env) {
+    if (env === 'production') {
+        return common;
+    }
+    if (env === 'development') {
+        return Object.assign(
+        {},
+        common,
+        developmentConfig
+        );
     }
 };
