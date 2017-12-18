f9986fb5b9d15b2ce9d86a68abb64916e9d76e14
AVKlimin <avklimin@yandex.ru>
2017-12-18 03:14:38 +0300

    Все переделал, верстка вся еще впереди

--- source/pages/index/index.js
+++
@@ -1,9 +0,0 @@
-import './index.scss';
-import 'normalize.css';
-import createMenu from '../../components/menu/menu';
-var menu = createMenu(['Мои работы','Обо мне','Блог'], 'menu');
-document.body.appendChild(menu);
-console.log('in index.js');
-console.log($);
-console.log(jQuery);
-
