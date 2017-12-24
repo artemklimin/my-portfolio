1201b254d1a9a84ef17538190bcd303261b5b35a
AVKlimin <avklimin@yandex.ru>
2017-12-24 15:31:15 +0300

    в работе

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,7 +1,7 @@
 const slider = require('./common/slider');
 const fsmenu = require('./common/fs-menu.js');
-const gmaps = require('./common/gmaps.js');
+//const gmaps = require('./common/gmaps.js');
 const $ = require('jquery');
 
 slider();
-gmaps();
+//gmaps();
